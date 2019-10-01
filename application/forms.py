8934315a5028d87363b9946a0f82087da646c01a
HiddenRealm <jamestaylor034@gmail.com>
2019-10-01 16:33:50 +0100

    added multiple pages for the selection screen. and got an understanding of how i want to go forward. also fixed my database with my team table.

--- application/forms.py
+++ application/forms.py
@@ -72,3 +72,7 @@ class UpdateAccountForm(FlaskForm):
 			user = Users.query.filter_by(email=email.data).first()
 			if user:
 				raise ValidationError('Email already in use!')	
+
+class SelectionForm(FlaskForm):
+	one = BooleanField('Loosehead Prop')
+	submit = SubmitField('Save!')
