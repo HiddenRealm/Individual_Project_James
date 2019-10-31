4bc09495f63a123091f7f799d26d9f0c7b94a43d
HiddenRealm <38515420+HiddenRealm@users.noreply.github.com>
2019-10-31 16:48:58 +0000

    Update download_docker.sh

--- download_docker.sh
+++ download_docker.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
-sudo apt-get install docker
-sudo apt-get install docker.io
+sudo apt-get install -y docker
+sudo apt-get install -y docker.io
 
 sudo usermod -aG docker $USER
 newgrp docker
