b48831689d48ae3dafb8087c2f91ccf3406e18d5
HiddenRealm <38515420+HiddenRealm@users.noreply.github.com>
2019-11-01 10:26:09 +0000

    Update systemd_build.sh

--- systemd_build.sh
+++ systemd_build.sh
@@ -1,1 +1,20 @@
+sudo useradd -m -s /bin/bash pythonadm
 
+sudo cp flask-app.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl stop flask-app
+
+install_dir=/opt/flask-app
+sudo rm -rf ${install_dir}
+sudo mkdir ${install_dir}
+sudo cp -r ./* ${install_dir}
+sudo chown -R pythonadm:pythonadm ${install_dir}
+
+sudo su - pythonadm << EOF
+cd ${install_dir}
+virtualenv -p python3 venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+EOF
+
+sudo systemctl start flask-app
